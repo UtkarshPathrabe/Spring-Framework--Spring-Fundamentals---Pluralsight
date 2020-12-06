c692f24a742be8f60bebc11cf85a559f8b5d50db
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-12-06 22:21:29 +0530

    Update SpeakerServiceImpl.java

--- conference-java/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ conference-java/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -3,6 +3,7 @@ package com.pluralsight.service;
 import com.pluralsight.model.Speaker;
 import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
@@ -10,7 +11,12 @@ public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository repository;
 
+    public SpeakerServiceImpl() {
+        System.out.println("SpeakerServiceImpl no args constructor");
+    }
+
     public SpeakerServiceImpl(SpeakerRepository speakerRepository) {
+        System.out.println("SpeakerServiceImpl repository constructor");
         repository = speakerRepository;
     }
 
@@ -19,7 +25,9 @@ public class SpeakerServiceImpl implements SpeakerService {
         return repository.findAll();
     }
 
+    @Autowired
     public void setRepository(SpeakerRepository repository) {
+        System.out.println("SpeakerServiceImpl setter");
         this.repository = repository;
     }
 }
