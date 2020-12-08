99ba8860e377c6b8459fae533f7ebddef90b3dc9
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-12-08 22:50:47 +0530

    Factory Bean Commit

--- conference-java/src/main/java/AppConfig.java
+++ conference-java/src/main/java/AppConfig.java
@@ -3,14 +3,29 @@
 //import com.pluralsight.service.SpeakerService;
 //import com.pluralsight.service.SpeakerServiceImpl;
 //import org.springframework.beans.factory.config.BeanDefinition;
-//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
+import com.pluralsight.util.CalendarFactory;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Calendar;
 //import org.springframework.context.annotation.Scope;
 
 @Configuration
 @ComponentScan({"com.pluralsight"})
 public class AppConfig {
+
+    @Bean(name = "cal")
+    public CalendarFactory calFactory() {
+        CalendarFactory factory = new CalendarFactory();
+        factory.addDays(2);
+        return factory;
+    }
+
+    @Bean
+    public Calendar cal() throws Exception {
+        return calFactory().getObject();
+    }
 
     /*
     @Bean(name = "speakerService")
--- conference-java/src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ conference-java/src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -1,13 +1,18 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Speaker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Repository("speakerRepository")
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+    @Autowired
+    private Calendar cal;
 
     @Override
     public List<Speaker> findAll() {
@@ -16,6 +21,7 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
         speaker.setFirstName("Utkarsh");
         speaker.setLastName("Pathrabe");
         speakers.add(speaker);
+        System.out.println("cal: " + cal.getTime());
         return speakers;
     }
 
