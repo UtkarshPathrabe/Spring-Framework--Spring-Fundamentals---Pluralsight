b26e9f87868f1f4e2ceedcd81d93c5cd423c3bc5
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-12-08 23:00:12 +0530

    SpEL Commit

--- conference-java/src/main/java/Application.java
+++ conference-java/src/main/java/Application.java
@@ -9,8 +9,9 @@ public class Application {
         ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
 //        SpeakerService service = new SpeakerServiceImpl();
         SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
-        System.out.println(service);
+//        System.out.println(service);
         System.out.println(service.findAll().get(0).getFirstName());
+        System.out.println(service.findAll().get(0).getSeedNum());
 //        SpeakerService service2 = appContext.getBean("speakerService", SpeakerService.class);
 //        System.out.println(service2);
     }
--- conference-java/src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ conference-java/src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.pluralsight.repository;
 
 import com.pluralsight.model.Speaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -14,12 +15,16 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
     @Autowired
     private Calendar cal;
 
+    @Value("#{T(java.lang.Math).random() * 100}")
+    private double seedNum;
+
     @Override
     public List<Speaker> findAll() {
         List<Speaker> speakers = new ArrayList<Speaker>();
         Speaker speaker = new Speaker();
         speaker.setFirstName("Utkarsh");
         speaker.setLastName("Pathrabe");
+        speaker.setSeedNum(seedNum);
         speakers.add(speaker);
         System.out.println("cal: " + cal.getTime());
         return speakers;
