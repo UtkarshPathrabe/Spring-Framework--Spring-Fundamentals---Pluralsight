86081cf14861d36e858195c9365ee5351a872a0b
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-12-08 20:51:57 +0530

    Update AppConfig.java

--- conference-java/src/main/java/AppConfig.java
+++ conference-java/src/main/java/AppConfig.java
@@ -1,15 +1,18 @@
-import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
-import com.pluralsight.repository.SpeakerRepository;
-import com.pluralsight.service.SpeakerService;
-import com.pluralsight.service.SpeakerServiceImpl;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.annotation.Bean;
+//import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
+//import com.pluralsight.repository.SpeakerRepository;
+//import com.pluralsight.service.SpeakerService;
+//import com.pluralsight.service.SpeakerServiceImpl;
+//import org.springframework.beans.factory.config.BeanDefinition;
+//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
+//import org.springframework.context.annotation.Scope;
 
 @Configuration
+@ComponentScan({"com.pluralsight"})
 public class AppConfig {
 
+    /*
     @Bean(name = "speakerService")
     @Scope(value = BeanDefinition.SCOPE_SINGLETON)
 //    @Scope(value = BeanDefinition.SCOPE_PROTOTYPE)
@@ -20,9 +23,11 @@ public class AppConfig {
         return service;
     }
 
+
     @Bean(name = "speakerRepository")
     public SpeakerRepository getSpeakerRepository() {
         return new HibernateSpeakerRepositoryImpl();
     }
+    */
 
 }
